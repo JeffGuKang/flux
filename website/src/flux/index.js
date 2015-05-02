c1ec019d566a0cb714b17c5ca7215c78039e899c
Edward Kim <onward.edkim@gmail.com>
2015-05-02 10:07:31 +1000

    translate to Korean - main page of website

--- website/src/flux/index.js
+++ website/src/flux/index.js
@@ -16,7 +16,7 @@ var index = React.createClass({
           <div className="wrap">
             <div className="text"><strong>Flux</strong></div>
             <div className="minitext">
-              Application Architecture for Building User Interfaces
+              사용자 인터페이스를 만들기 위한 어플리케이션 아키텍쳐
             </div>
           </div>
         </div>
@@ -24,7 +24,7 @@ var index = React.createClass({
         <section className="content wrap">
           <section className="home-section home-getting-started">
             <p>
-              Flux is the application architecture that Facebook uses for building client-side web applications.  It complements React's composable view components by utilizing a unidirectional data flow.  It's more of a pattern rather than a formal framework, and you can start using Flux immediately without a lot of new code.
+              Flux는 Facebook에서 클라이언트-사이드 웹 어플리케이션을 만들기 위해 사용하는 어플리케이션 아키텍쳐다. 단방향 데이터 흐름을 활용하는 React의 구성형 뷰 컴포넌트를 보완한다. 이전의 프레임워크와 다르게 패턴에 가깝기 때문에, 새로운 많은 코드를 작성하는 과정 없이 Flux를 바로 사용할 수 있다.
             </p>
           </section>
 
@@ -34,7 +34,7 @@ var index = React.createClass({
 
           <section className="home-bottom-section">
             <div className="buttons-unit">
-              <a href="docs/overview.html#content" className="button">Learn more about Flux</a>
+              <a href="docs/overview.html#content" className="button">Flux에 대해 더 알아보기</a>
             </div>
           </section>
           <p></p>
