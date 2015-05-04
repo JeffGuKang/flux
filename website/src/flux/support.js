de630ff8bc57ec895f87b5bd15edd16503247b9d
Edward Kim <onward.edkim@gmail.com>
2015-05-04 23:28:41 +1000

    add translate information on support page

--- website/src/flux/support.js
+++ website/src/flux/support.js
@@ -14,23 +14,26 @@ var support = React.createClass({
 
         <section className="content wrap documentationContent nosidebar">
           <div className="inner-content">
-            <h1>Need help?</h1>
+            <h1>도움이 필요하세요?</h1>
             <div className="subHeader"></div>
             <p>
-              <strong>Flux</strong> is worked on full-time by Facebook&#39;s
+              <em>번역 주 :</em> 이 문서의 번역은 Github의 <a href="https://github.com/haruair/flux" target="_blank">haruair/flux</a> 리포지터리에서 관리되고 있습니다. 번역에 대한 문의, 의견 및 건의는 <a href="https://github.com/haruair/flux/issues" target="_blank">Issue</a>를 활용해주세요.
+            </p>
+            
+            <p>
+              <strong>Flux</strong>는 Facebook의 
               product infrastructure user interface
-              engineering teams. They&#39;re often around and available for
-              questions.
+              engineering 팀이 풀타임으로 작업하고 있습니다. 팀원은 다음의 체널들을 둘러보고 질문에 답변을 합니다.
             </p>
 
             <H2>Stack Overflow</H2>
-            <p>Many members of the community use Stack Overflow to ask questions. Read through the <a href="http://stackoverflow.com/questions/tagged/reactjs">existing questions</a> tagged with <strong>reactjs</strong> or <a href="http://stackoverflow.com/questions/ask">ask your own</a>!</p>
-            <H2>Google Groups mailing list</H2>
-            <p><a href="http://groups.google.com/group/reactjs" target="_blank">The <strong>reactjs</strong> Google Group</a> is also a good place to ask questions and find answers.</p>
+            <p>커뮤니티의 많은 맴버가 질문을 위해 Stack Overflow를 사용합니다. <strong>reactjs</strong>가 태그된 <a href="http://stackoverflow.com/questions/tagged/reactjs">존재하는 질문</a>을 살펴보거나 <a href="http://stackoverflow.com/questions/ask">직접 질문해보세요</a>!</p>
+            <H2>Google 그룹스 메일링 리스트</H2>
+            <p><a href="http://groups.google.com/group/reactjs" target="_blank"><strong>reactjs</strong> 구글 그룹</a>도 질문과 답변을 얻기 좋은 곳입니다.</p>
             <H2>IRC</H2>
-            <p>Many developers and users idle on Freenode.net&#39;s IRC network in <strong><a href="irc://chat.freenode.net/reactjs">#reactjs on freenode</a></strong>.</p>
+            <p>많은 개발자와 사용자가 Freenode.net&#39; IRC 네트워크의 <strong><a href="irc://chat.freenode.net/reactjs">#reactjs</a></strong> 체널에 상주하고 있습니다.</p>
             <H2>Twitter</H2>
-            <p><a href="https://twitter.com/search?q=%23fluxjs"><strong>#fluxjs</strong> hash tag on Twitter</a> is used to keep up with the latest Flux news.</p>
+            <p><a href="https://twitter.com/search?q=%23fluxjs"><strong>#fluxjs</strong> 트위터의 해시태그</a>로 최신 Flux 뉴스를 확인할 수 있습니다.</p>
 
             <p><Center><a className="twitter-timeline" data-dnt="true" data-chrome="nofooter noheader transparent" href="https://twitter.com/search?q=%23fluxjs" data-widget-id="493727680810848257"></a></Center></p>
           </div>
